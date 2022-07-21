84fbc6d7d0b70fd1312380b0bdc812bc6d8803d2
jmceager <102826484+jmceager@users.noreply.github.com>
2022-07-21 12:30:39 -0700

    update: conditional cells in reactable

--- nrc-review/nrc-review/app.R
+++ nrc-review/nrc-review/app.R
@@ -15,14 +15,19 @@ cols <- c("SEQNOS", "LOC_FULL", "RESPONSIBLE_COMPANY","INC_DATE", "INCIDENT_DATE
           "ANY_FATALITIES","NUMBER_FATALITIES","NUMBER_EVACUATED",
           "lat","lon", "SYS", "size", "DESCRIPTION_OF_INCIDENT")
 ## real time data 
-#df <- nrcGeo("https://nrc.uscg.mil/FOIAFiles/Current.xlsx") %>%
-#  select(all_of(cols))
+## TODO: Set up conditional daily data cleaning (i.e. only geocode when maxdate is newer)
+df <- nrcGeo("https://nrc.uscg.mil/FOIAFiles/Current.xlsx") %>%
+   select(all_of(cols)) %>%
+   mutate(DESCRIPTION_OF_INCIDENT = gsub('[\r\n]', '', DESCRIPTION_OF_INCIDENT))
 ## write to csv for testing 
 # write.csv(df, file = "testing.csv")
 ## reading testing data
-df <- read_csv("testing.csv")
-df <- df[,2:length(df)] %>%
-  mutate(INCIDENT_DATE_TIME = format(INCIDENT_DATE_TIME, format = "%H:%M:%S"))
+# df <- read_csv("testing.csv")
+# df <- df[,2:length(df)] %>%
+#   mutate(INCIDENT_DATE_TIME = format(INCIDENT_DATE_TIME, format = "%H:%M:%S"),
+#          DESCRIPTION_OF_INCIDENT = gsub('[\r\n]', '', DESCRIPTION_OF_INCIDENT))
+
+
 
 ds <- stamp("8 March, 2022")
 
@@ -75,7 +80,11 @@ ui <- fluidPage(
                conditionalPanel(condition = "input.time == 2",
                                 numericInput(inputId = "week",
                                              label = h5("# of Weeks"),
-                                             value = 2)),
+                                             value = 2,
+                                             min = 1, 
+                                             max = as.numeric(difftime(max(df$INC_DATE), min(df$INC_DATE), units = "weeks")),
+                                             step = 1)
+                                ),
                hr()
                ), #close column
 
@@ -209,18 +218,72 @@ server <- function(input, output) {
                 NAME_OF_MATERIAL = colDef(name = "Material"),
                 MEDIUM_DESC = colDef(name = "Release Medium"),
                 ADDITIONAL_MEDIUM_INFO = colDef(name = "Medium Info", show = F),
-                ANY_DAMAGES = colDef(name = "Damage?", show = F),
+                ANY_DAMAGES = colDef(name = "Damage?", show = T,
+                                     cell = function(value, index) {
+                                       dmg <- tdf$DAMAGE_AMOUNT[index]
+                                       dmg <- if (!is.na(dmg)) scales::dollar(dmg, scale = .001, suffix = "K")  else "U"
+                                       div(
+                                         div(style = if_else(value == "Y", 
+                                                             "font-weight: 600",
+                                                             "font-weight: 400"),
+                                             if_else(value == "Y",
+                                                     dmg,
+                                                     value)
+                                             )
+                                       )
+                                     }),
                 DAMAGE_AMOUNT = colDef(name = "Damage Cost", format = colFormat(currency = "USD"), show = F),
                 AMOUNT_OF_MATERIAL = colDef(name = "Amount Released",
-                                       format = colFormat(separators = TRUE),
-                                       minWidth = 100),
+                                            minWidth = 100,
+                                            cell = function(value, index) {
+                                              unit <- tdf$UNIT_OF_MEASURE[index]
+                                              valForm <- ifelse(value > 1000, scales::comma(value), value)
+                                              div(
+                                                div(style = if_else(value > 0 , 
+                                                                    "font-weight: 600",
+                                                                    "font-weight: 400"),
+                                                    ifelse(value > 0 ,
+                                                            valForm,
+                                                            unit)
+                                                    ), # inner div 1 
+                                                div( style = "font-size: .9rem",
+                                                     ifelse(value != 0, unit, "")
+                                                )
+                                              )
+                                            }),
                 INCIDENT_CAUSE = colDef(name = "Cause", minWidth = 100),
-                UNIT_OF_MEASURE = colDef(name = "Release Unit", minWidth = 110),
+                UNIT_OF_MEASURE = colDef(name = "Release Unit", minWidth = 110, show = F),
                 FIRE_INVOLVED = colDef(name = "Fire?"),
-                ANY_INJURIES = colDef(name = "Injuries?"),
+                ANY_INJURIES = colDef(name = "Injuries?",
+                                      cell = function(value, index) {
+                                        inj <- tdf$NUMBER_INJURED[index]
+                                        inj <- if (!is.na(inj)) as.character(inj) else ""
+                                        div(
+                                          div(style = if_else(value == "Y", 
+                                                              "font-weight: 600",
+                                                              "font-weight: 400"),
+                                              if_else(value == "Y",
+                                                      inj,
+                                                      value)
+                                          )
+                                        )
+                                      }),
                 NUMBER_INJURED = colDef(name = "Injured", show = F),
                 NUMBER_HOSPITALIZED = colDef(name = "Hospitalized", show = F),
-                ANY_FATALITIES = colDef(name = "Fatal?"),
+                ANY_FATALITIES = colDef(name = "Fatal?",
+                                        cell = function(value, index) {
+                                          ftl <- tdf$NUMBER_FATALITIES[index]
+                                          ftl <- if (!is.na(ftl)) as.character(ftl) else ""
+                                          div(
+                                            div(style = if_else(value == "Y", 
+                                                                "font-weight: 600",
+                                                                "font-weight: 400"),
+                                                if_else(value == "Y",
+                                                        ftl,
+                                                        value)
+                                            )
+                                          )
+                                        }),
                 NUMBER_FATALITIES = colDef(name = "Fatalities", show = F),
                 NUMBER_EVACUATED = colDef(name = "Evacuations", show = F),
                 lat = colDef(show = F),
@@ -229,16 +292,18 @@ server <- function(input, output) {
                 size = colDef(show = F),
                 selected = colDef(show = F),
                 details = colDef(name = "More",
-                                 details = JS("function(rowInfo) {
-                                                return `Details for row: ${rowInfo.index}` +
-                                                `<pre>${JSON.stringify(rowInfo.values['NUMBER_INJURED'], null, 2)}</pre>`
-                                              }"),
-                                 html = TRUE,
-                                 width = 60
+                                 details = function(index){
+                                   paste(
+                                     "Details for Incident SEQNOS: ", tdf[index, "SEQNOS"], "\n",
+                                     tdf[index, "NUMBER_EVACUATED"]
+                                   )
+                                 },
+                                 width = 60,
+                                 show = F
                                 ),
                 DESCRIPTION_OF_INCIDENT = colDef(show = F),
                 description = colDef(name = "Narrative", 
-                                     cell = function() htmltools::tags$button("Narrative") )
+                                     cell = function() htmltools::tags$button("Read More") )
                 ),
               onClick = JS("function(rowInfo, column) {
                             // Only handle click events on the 'details' column
