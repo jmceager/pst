71dc443f74a1dc9a120927959bf5039521d5753d
jmceager <102826484+jmceager@users.noreply.github.com>
2022-07-25 11:57:24 -0700

    refactor: dl xl, condition geocode on date

--- nrc-review/nrc-review/nrc_geocode.R
+++ nrc-review/nrc-review/nrc_geocode.R
@@ -3,59 +3,32 @@ library(tidyverse)
 library(readxl)
 library(lubridate)
 
+#write data to OneDrive
+outPath <- "/Users/jameseager/Library/CloudStorage/OneDrive-pstrust.org/data/nrc"
+
 #list of interested materials
 materials <- c("CRUDE", "GAS", "PETROLEUM","KEROSENE","ETHYLENE","DIESEL", "OIL")
+
+#wanted cols
+cols <- c("SEQNOS", "LOC_FULL", "RESPONSIBLE_COMPANY","INC_DATE", "INCIDENT_DATE_TIME",
+          "INCIDENT_CAUSE","NAME_OF_MATERIAL", "AMOUNT_OF_MATERIAL","UNIT_OF_MEASURE",
+          "MEDIUM_DESC","ADDITIONAL_MEDIUM_INFO","ANY_DAMAGES","DAMAGE_AMOUNT",
+          "FIRE_INVOLVED","ANY_INJURIES","NUMBER_INJURED","NUMBER_HOSPITALIZED",
+          "ANY_FATALITIES","NUMBER_FATALITIES","NUMBER_EVACUATED",
+          "lat","lon", "SYS", "size", "DESCRIPTION_OF_INCIDENT")
 
 #sizes for systems 
 gasBreaks <- c(0, 40, 1000, 8000, 600000)
 liqBreaks <- c(0, 20, 100, 1000, 5000000)
 breakLab <- c(4,8,12,16)
 
-nrcGeo <- function(path){
-  temp = tempfile(fileext = ".xlsx")
-  download.file(path, destfile=temp, mode='wb')
-  
-  pipes <- read_xlsx(temp, sheet = 2) %>%
-    mutate(
-      INCIDENT_DATE_TIME = parse_date_time(INCIDENT_DATE_TIME, orders = "mdY HM"),
-      INC_DATE = dmy(paste(day(INCIDENT_DATE_TIME), month(INCIDENT_DATE_TIME), year(INCIDENT_DATE_TIME), sep = "-")),
-      LOC_FIRST = coalesce(LOCATION_NEAREST_CITY, LOCATION_COUNTY),
-      LOC_FULL = paste(LOC_FIRST, LOCATION_STATE, sep = ", ")
-    ) %>%
-   # filter(INC_DATE >= ymd()) %>%
-    left_join( read_xlsx(temp, sheet = 3), by = "SEQNOS") %>% 
-    filter(TYPE_OF_INCIDENT == "PIPELINE") %>%
-    mutate(human = if_else( ANY_EVACUATIONS == "Y" | 
-                              ANY_INJURIES == "Y" |
-                              ANY_FATALITIES == "Y",
-                            "H",
-                            "NA"),
-           dmg = if_else(ANY_DAMAGES == "Y"|
-                           FIRE_INVOLVED == "Y",
-                         "D",
-                         "NA"),
-           hd = paste0(human, dmg),
-           hd = str_replace(hd, "NANA", "None"),
-           hd = str_remove(hd, "NA"))   %>%    
-    left_join( read_xlsx(temp, sheet = 3), by = "SEQNOS", suffix = c("",".x"))%>%    #incident details
-    left_join( read_xlsx(temp, sheet = 4), by = "SEQNOS", suffix = c("",".y"))%>%    #incident details
-    left_join( read_xlsx(temp, sheet = 5), by = "SEQNOS", suffix = c("",".z"))%>%   # material details 
-    left_join( read_xlsx(temp, sheet = 1) %>% select(SEQNOS, RESPONSIBLE_COMPANY), #operator info 
-               by = "SEQNOS", suffix = c("",".c"))%>%
-    filter((hd != "None" | MEDIUM_DESC == "WATER") & 
-             grepl(paste(materials, collapse="|"), NAME_OF_MATERIAL, fixed = F)) %>%
-    mutate(NUMBER_EVACUATED = replace_na(NUMBER_EVACUATED,0),
-           NUMBER_INJURED = replace_na(NUMBER_INJURED, 0),
-           NUMBER_FATALITIES = replace_na(NUMBER_FATALITIES, 0),
-           PIPELINE_TYPE = str_trunc(PIPELINE_TYPE,8),
-           BODY_OF_WATER = replace_na(BODY_OF_WATER, "None"),
-           protocol = if_else(grepl("///",DESCRIPTION_OF_INCIDENT),"Y","Z"),
-           SEQNOS = str_sub(as.character(SEQNOS), -3))%>%
-    arrange(INC_DATE, protocol)%>%
-    distinct(LOC_FULL,INC_DATE, .keep_all = T)
-  
-  pipes_geocode <- geocode(pipes$LOC_FULL)
-  pipes_geocode <- cbind(pipes, pipes_geocode)
+# current df 
+current <- read_csv(paste0(outPath, "/current.csv"))
+curDate <- max(current$INC_DATE)
+
+nrcGeo <- function(df){
+  pipes_geocode <- geocode(df$LOC_FULL)
+  pipes_geocode <- cbind(df, pipes_geocode)
   pipes_geocode <- pipes_geocode %>%
     mutate(lat = if_else(is.na(lat),
                          LAT_DEG + (LAT_MIN/60) + (LAT_SEC/3600),
@@ -111,3 +84,66 @@ addLegendCustom <- function(map,title, legName , position, opacity = 0.7){
                    position = position))
   
 }
+
+
+dfUpdate <- function(p, c, d){ #path, current, curDate 
+  #load up the excel 
+  temp = tempfile(fileext = ".xlsx")
+  download.file(p, destfile=temp, mode='wb')
+  
+  pipes <- read_xlsx(temp, sheet = 2) %>%
+    mutate(
+      INCIDENT_DATE_TIME = parse_date_time(INCIDENT_DATE_TIME, orders = "mdY HM"),
+      INC_DATE = dmy(paste(day(INCIDENT_DATE_TIME), month(INCIDENT_DATE_TIME), year(INCIDENT_DATE_TIME), sep = "-")),
+      LOC_FIRST = coalesce(LOCATION_NEAREST_CITY, LOCATION_COUNTY),
+      LOC_FULL = paste(LOC_FIRST, LOCATION_STATE, sep = ", ")
+    ) %>%
+    # filter(INC_DATE >= ymd()) %>%
+    left_join( read_xlsx(temp, sheet = 3), by = "SEQNOS") %>% 
+    filter(TYPE_OF_INCIDENT == "PIPELINE") %>%
+    mutate(human = if_else( ANY_EVACUATIONS == "Y" | 
+                              ANY_INJURIES == "Y" |
+                              ANY_FATALITIES == "Y",
+                            "H",
+                            "NA"),
+           dmg = if_else(ANY_DAMAGES == "Y"|
+                           FIRE_INVOLVED == "Y",
+                         "D",
+                         "NA"),
+           hd = paste0(human, dmg),
+           hd = str_replace(hd, "NANA", "None"),
+           hd = str_remove(hd, "NA"))   %>%    
+    left_join( read_xlsx(temp, sheet = 3), by = "SEQNOS", suffix = c("",".x"))%>%    #incident details
+    left_join( read_xlsx(temp, sheet = 4), by = "SEQNOS", suffix = c("",".y"))%>%    #incident details
+    left_join( read_xlsx(temp, sheet = 5), by = "SEQNOS", suffix = c("",".z"))%>%   # material details 
+    left_join( read_xlsx(temp, sheet = 1) %>% select(SEQNOS, RESPONSIBLE_COMPANY), #operator info 
+               by = "SEQNOS", suffix = c("",".c"))%>%
+    filter((hd != "None" | MEDIUM_DESC == "WATER") & 
+             grepl(paste(materials, collapse="|"), NAME_OF_MATERIAL, fixed = F)) %>%
+    mutate(NUMBER_EVACUATED = replace_na(NUMBER_EVACUATED,0),
+           NUMBER_INJURED = replace_na(NUMBER_INJURED, 0),
+           NUMBER_FATALITIES = replace_na(NUMBER_FATALITIES, 0),
+           PIPELINE_TYPE = str_trunc(PIPELINE_TYPE,8),
+           protocol = if_else(grepl("///",DESCRIPTION_OF_INCIDENT),"Y","Z"),
+           SEQNOS = str_sub(as.character(SEQNOS), -3),
+           DESCRIPTION_OF_INCIDENT = gsub('[\r\n]', '', DESCRIPTION_OF_INCIDENT))%>%
+    arrange(INC_DATE, protocol)%>%
+    distinct(LOC_FULL,INC_DATE, .keep_all = T)
+  #if most recent date is newer, update df 
+  if(max(pipes$INC_DATE) > d){
+    pg <- nrcGeo(pipes) %>%
+      select(all_of(cols))
+  }
+  # else return old df 
+  else{
+    pg <- c
+  }
+  return(pg)
+}
+
+dfUpdate("https://nrc.uscg.mil/FOIAFiles/Current.xlsx", c = current, d = curDate) %>%
+  write_csv(paste0(outPath, "/current.csv"))
+
+
+
+
