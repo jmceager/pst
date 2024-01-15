e87e3a2dbaedd7a5d7959cc3e8b779857fe48525
jmceager <jmc.eager@gmail.com>
2024-01-14 19:06:55 -0800

    update: editing approach
    part of a broader approach of fixing bugs and accuracy issues related to operator joins, offshore incidents, and location cleaning

--- phmsa-clean/phmsa_tables.R
+++ phmsa-clean/phmsa_tables.R
@@ -2,7 +2,11 @@ library(tidyverse)
 library(lutz)
 library(lubridate)
 library(readxl)
+library(fuzzyjoin)
 
+library(httr)
+
+httr::set_config(config(http_version = 1.1))
 source("offshorefinder.R")
 
 #### Data Setup ####
@@ -10,7 +14,7 @@ source("offshorefinder.R")
 ## TODO: replace 2022 miles with 2021 numbers 
 
 temp <- tempfile()
-download.file("https://www.phmsa.dot.gov/sites/phmsa.dot.gov/files/data_statistics/pipeline/PHMSA_Pipeline_Safety_Flagged_Incidents.zip", temp)
+download.file("https://www.phmsa.dot.gov/sites/phmsa.dot.gov/files/data_statistics/pipeline/PHMSA_Pipeline_Safety_Flagged_Incidents.zip", destfile =  temp, method = "curl")
 distData <- read_xlsx(unzip(temp, files = "gd2010toPresent.xlsx"), sheet = 2)
 tranData <- read_xlsx(unzip(temp, files = "gtggungs2010toPresent.xlsx"), sheet = 2)
 hzrdData <- read_xlsx(unzip(temp, files = "hl2010toPresent.xlsx"), sheet = 2)
@@ -67,21 +71,36 @@ ops<-safe %>%
          sub.name = SUBMITTING_OPID_NAME,
          sub.status = SUBMITTING_OPID_STATUS,
          sub.sys = SYSTEM_TYPE,
-         pri.id = `D&A_PRIMARY_OPID`,
-         pri.id = coalesce(OME_PRIMARY_OPID, `D&A_PRIMARY_OPID`, 
-                           IM_PRIMARY_OPID, OPA_PRIMARY_OPID,
-                           PA_PRIMARY_OPID, DP_PRIMARY_OPID, 
-                           OQ_PRIMARY_OPID, CRM_PRIMARY_OPID),
-         pri.name = coalesce(OME_PRIMARY_NAME, `D&A_PRIMARY_NAME`, 
-                             IM_PRIMARY_NAME, OPA_PRIMARY_NAME,
-                             PA_PRIMARY_NAME, DP_PRIMARY_NAME, 
-                             OQ_PRIMARY_NAME, CRM_PRIMARY_NAME),
-         pri.status = coalesce(OME_STATUS, `D&A_STATUS`, 
-                               IM_STATUS, OPA_STATUS,
-                               PA_STATUS, DP_STATUS, 
-                               OQ_STATUS, CRM_STATUS)
+         pri.id = coalesce(#OME_PRIMARY_OPID, `D&A_PRIMARY_OPID`, 
+                           IM_PRIMARY_OPID#, OPA_PRIMARY_OPID,
+                           #PA_PRIMARY_OPID, DP_PRIMARY_OPID, 
+                           #OQ_PRIMARY_OPID, CRM_PRIMARY_OPID
+                           ),
+         pri.name = coalesce(#OME_PRIMARY_NAME, `D&A_PRIMARY_NAME`, 
+                             IM_PRIMARY_NAME#, OPA_PRIMARY_NAME,
+                             # PA_PRIMARY_NAME, DP_PRIMARY_NAME, 
+                             # OQ_PRIMARY_NAME, CRM_PRIMARY_NAME
+                             ),
+         pri.status = coalesce(#OME_STATUS, `D&A_STATUS`, 
+                               IM_STATUS#, OPA_STATUS,
+                               # PA_STATUS, DP_STATUS, 
+                               # OQ_STATUS, CRM_STATUS
+                               ),
+         start = mdy(
+                  coalesce(#OME_ENTER_DATE, `D&A_ENTER_DATE`, 
+                           IM_ENTER_DATE#, OPA_ENTER_DATE,
+           #                 PA_ENTER_DATE, DP_ENTER_DATE, 
+           #                 OQ_ENTER_DATE, CRM_ENTER_DATE
+           )
+            ),
+         end = mdy(
+               #coalesce(OME_EXIT_DATE, `D&A_EXIT_DATE`, 
+                        IM_EXIT_DATE#, OPA_EXIT_DATE,
+                        #PA_EXIT_DATE, DP_EXIT_DATE, 
+                        #OQ_EXIT_DATE, CRM_EXIT_DATE)
+               )
   )%>%
-  select(sub.id,sub.name,sub.sys,sub.status,pri.id,pri.name, pri.status) %>%
+  select(sub.id,sub.name,sub.sys,sub.status,pri.id,pri.name, pri.status, start,end) %>%
   distinct() %>%
   mutate(pri.name = if_else(str_detect(pri.name,regex("enbridge",ignore_case = TRUE)),
                             "Enbridge",pri.name
@@ -92,10 +111,15 @@ ops<-safe %>%
   pri.name = if_else(str_detect(pri.name,regex("kinder morgan",ignore_case = TRUE)),
                      "Kinder Morgan",pri.name
   ),
+  pri.name = if_else(str_detect(pri.name,regex("PGandE",ignore_case = TRUE)),
+                     "PG&E",pri.name
+  ),
   sub.sys = str_extract_all(sub.sys, pattern = "[A-Z]") %>% #extract capital letters
     sapply(.,paste,collapse = "")%>%   #paste list into sys abbreviations
-    str_replace(.,"PF","") # fix lngpf to lng
-  )
+    str_replace(.,"PF",""), # fix lngpf to lng,
+  end = if_else(pri.status == "Current",ymd(Sys.Date()), end)
+  ) %>%
+  filter(!is.na(pri.status))
 
 ops.simple <- ops %>%
   select(sub.id, sub.name, sub.sys, pri.id, pri.name)
@@ -122,16 +146,32 @@ gd.full <- distData %>%
          MSYS = "Gas",
          ILOC = paste(str_to_title(LOCATION_CITY_NAME),LOCATION_STATE_ABBREVIATION, sep = ", "),
          STATE = LOCATION_STATE_ABBREVIATION)%>%
-  locCleaner(., loc = "ILOC", lat= "LOCATION_LATITUDE", lon = "LOCATION_LONGITUDE", state = "STATE")%>%
+  #locCleaner(., loc = "ILOC", lat= "LOCATION_LATITUDE", lon = "LOCATION_LONGITUDE", state = "STATE")%>%
   left_join(miles, by = c("OPERATOR_ID", "SYS", "STATE","IYEAR"))%>%
   mutate(mileage = replace_na(mileage, 0)) %>%
-  left_join(filter(ops.simple, sub.sys == "GD"), by = c("OPERATOR_ID" = "sub.id"))%>% 
+  #left_join(filter(ops.simple, sub.sys == "GD"), by = c("OPERATOR_ID" = "sub.id"))%>% 
   distinct(NARRATIVE, .keep_all = T)%>%
   rename(NAME = NAME.x)
 
+# gd.op <- tibble()
+# for(i in 1:nrow(gd.full)){
+#   row <- gd.full[i,]
+#   if(row$OPERATOR_ID %in% ops$sub.id){
+#     ops_match <- filter(ops, sub.id == row$OPERATOR_ID, 
+#                              sub.sys == row$SYS, 
+#                              start <= row$MDY,
+#                              end > row$MDY)
+#     row_match <- left_join(row, ops_match, by = c("OPERATOR_ID" = "sub.id"))
+#   }
+#   else {
+#     row_match <- row
+#   }
+#   gd.op <- bind_rows(gd.op, row_match)
+# }
+
 
 ## add safety data to mileage for consistency 
-miles <- left_join(miles, ops.simple, by = c("OPERATOR_ID" = "sub.id")) 
+#miles <- left_join(miles, ops.simple, by = c("OPERATOR_ID" = "sub.id")) 
 
 # gt big
 gt.full <- tranData %>%
@@ -163,15 +203,35 @@ gt.full <- tranData %>%
                          "OCS",
                          STATE)
     )%>%
-  locCleaner(.,"ILOC","LOCATION_LATITUDE","LOCATION_LONGITUDE", "OFF_ACCIDENT_ORIGIN", "STATE")%>%
+  #locCleaner(.,"ILOC","LOCATION_LATITUDE","LOCATION_LONGITUDE", "OFF_ACCIDENT_ORIGIN", "STATE")%>%
   left_join(miles, by = c("OPERATOR_ID", "SYS", "STATE","IYEAR"))%>%
   mutate(mileage = replace_na(mileage, 0))%>%
-  left_join(filter(ops.simple, sub.sys == "GT"), by = c("OPERATOR_ID" = "sub.id"))%>% 
-  distinct(NARRATIVE, .keep_all = T)%>%
-  rename(pri.id = pri.id.x,
-         pri.name = pri.name.x,
-         NAME = NAME.x)
+  #left_join(filter(ops.simple, sub.sys == "GT"), by = c("OPERATOR_ID" = "sub.id"))%>% 
+  distinct(NARRATIVE, .keep_all = T)
   
+# gt.op <- tibble()
+# for(i in 1:nrow(gt.full)){
+#   row <- gt.full[i,]
+#   if(row$OPERATOR_ID %in% ops$sub.id){
+#     ops_match <- filter(ops, sub.id == row$OPERATOR_ID, 
+#                         sub.sys == row$SYS, 
+#                         start <= row$MDY,
+#                         end > row$MDY)
+#     row_match <- left_join(row, ops_match, by = c("OPERATOR_ID" = "sub.id"))
+#   }
+#   else {    
+#     empty_op <- tibble(sub.sys = "GT",
+#                        sub.status = NA,
+#                        pri.id = NA,
+#                        pri.name = NA,
+#                        pri.status = NA,
+#                        start = NA,
+#                        end = NA)
+#     row_match <- cbind(row, empty_op)
+#   }
+#   gt.op <- rbind(gt.op, row_match)
+# }
+
 
 # hl big
 hl.full <- hzrdData %>% 
@@ -203,34 +263,66 @@ hl.full <- hzrdData %>%
                          "OCS",
                          STATE) 
         )%>%
-  locCleaner(.,"ILOC","LOCATION_LATITUDE","LOCATION_LONGITUDE", "OFF_ACCIDENT_ORIGIN", "STATE")%>%
+  #locCleaner(.,"ILOC","LOCATION_LATITUDE","LOCATION_LONGITUDE", "OFF_ACCIDENT_ORIGIN", "STATE")%>%
   left_join(miles, by = c("OPERATOR_ID", "SYS", "STATE","IYEAR"))%>%
   mutate(mileage = replace_na(mileage, 0))%>%
-  left_join(filter(ops.simple, sub.sys == "HL"), by = c("OPERATOR_ID" = "sub.id"))%>% 
-  distinct(NARRATIVE, .keep_all = T)%>%
-  rename(pri.id = pri.id.x,
-         pri.name = pri.name.x,
-         NAME = NAME.x)
+  #left_join(filter(ops.simple, sub.sys == "HL"), by = c("OPERATOR_ID" = "sub.id"))%>% 
+  distinct(NARRATIVE, .keep_all = T)
 
+# hl.op <- tibble()
+# for(i in 1:nrow(hl.full)){
+#   row <- hl.full[i,]
+#   if(row$OPERATOR_ID %in% ops$sub.id){
+#     ops_match <- filter(ops, sub.id == row$OPERATOR_ID, 
+#                         sub.sys == row$SYS, 
+#                         start <= row$MDY,
+#                         end > row$MDY)
+#     row_match <- left_join(row, ops_match, by = c("OPERATOR_ID" = "sub.id"))
+#   }
+#   else {
+#     row_match <- row
+#   }
+#   hl.op <- bind_rows(hl.op, row_match)
+# }
+
+# 
+# miles.op <- tibble()
+# for(i in 1:nrow(miles)){
+#   row <- miles[i,]
+#   row$MDY <- year( mdy(paste0("01-01-", row$IYEAR)) ) # turn year into jan 1st date
+#   if(row$OPERATOR_ID %in% ops$sub.id){
+#     ops_match <- filter(ops, sub.id == row$OPERATOR_ID, 
+#                         sub.sys == row$SYS, 
+#                         start <= row$MDY,
+#                         end > row$MDY)
+#     row_match <- left_join(row, ops_match, by = c("OPERATOR_ID" = "sub.id"))
+#   }
+#   else {
+#     row_match <- row
+#   }
+#   miles.op <- bind_rows(miles.op, row_match)
+# }
 
 #### JOINS, BINDS, RATE ####
 
 #columns for abridged incidents
 short_cols <- c( "REPORT_NUMBER", "NAME","OPERATOR_ID",  #basic characteristics
                  "IYEAR","MDY","MoYr" ,"LOCAL_DATETIME" ,   #temporal char
-                 "LOCATION_LATITUDE","LOCATION_LONGITUDE", "cleanLoc", "STATE", "ON_OFF_SHORE",#location
+                 "LOCATION_LATITUDE","LOCATION_LONGITUDE", #"cleanLoc", 
+                 "STATE", "ON_OFF_SHORE",#location
                  "SYS","MSYS" ,"SIGNIFICANT", "SERIOUS","COMMODITY_RELEASED_TYPE",#inc summary
                  "UNINTENTIONAL_RELEASE", "INTENTIONAL_RELEASE", "TOTAL_RELEASE","UNITS", #releases
                  "FATALITY_IND","FATAL", "INJURY_IND","INJURE", #human impact
                  "EXPLODE_IND","IGNITE_IND" ,  "NUM_PUB_EVACUATED", "TOTAL_COST_CURRENT",#impact 2
                  "INSTALLATION_YEAR", "SYSTEM_PART_INVOLVED", "PIPE_DIAMETER", #inc char 
                  "CAUSE","CAUSE_DETAILS", "NARRATIVE", #inc char 
-                 "mileage", "pri.id","pri.name")  #joined char
+                 "mileage"#, "pri.id","pri.name"
+                 )  #joined char
 
 #abridged all inc  
-all.inc <- rbind(select(hl.full, all_of(short_cols)), 
-                 select(gt.full, all_of(short_cols)), 
-                 select(gd.full, all_of(short_cols))) 
+all.inc <- rbind(select(hl.full%>%  rename(NAME = NAME.x), all_of(short_cols)), 
+                 select(gt.full %>%  rename(NAME = NAME.x), all_of(short_cols)), 
+                 select(gd.full, all_of(short_cols)))
 
 
 ## get incident rate per operator (by base ID not prime/sub)
@@ -238,17 +330,17 @@ all.inc <- rbind(select(hl.full, all_of(short_cols)),
 operatorRate <- miles %>%
   group_by(OPERATOR_ID, SYS, IYEAR)%>%
   summarize(miles = sum(mileage))%>%
-  full_join(hl.full %>%
+  full_join(hl.op %>%
               count(OPERATOR_ID, IYEAR) %>%
               mutate(SYS = "HL"),
              by = c("OPERATOR_ID", "IYEAR","SYS")
             )%>% 
-  full_join(gt.full %>%
+  full_join(gt.op %>%
                count(OPERATOR_ID, IYEAR) %>%
                mutate(SYS = "GT"),
             by = c("OPERATOR_ID", "IYEAR","SYS")
   )%>%
-  full_join(gd.full %>%
+  full_join(gd.op %>%
                count(OPERATOR_ID, IYEAR) %>%
                mutate(SYS = "GD"),
             by = c("OPERATOR_ID", "IYEAR","SYS")
@@ -271,12 +363,12 @@ operatorRate <- miles %>%
 all.inc <- left_join(all.inc, operatorRate, 
                      by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
 
-gt.full <- left_join(gt.full, operatorRate, 
-                     by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
-gd.full <- left_join(gd.full, operatorRate, 
-                     by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
-hl.full <- left_join(hl.full, operatorRate, 
-                     by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
+gt.op <- left_join(gt.op, operatorRate, 
+                   by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
+gd.op <- left_join(gd.op, operatorRate, 
+                   by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
+hl.op <- left_join(hl.op, operatorRate, 
+                   by = c("OPERATOR_ID", "SYS"), suffix = c("",".or"))
 
 
 
@@ -288,21 +380,21 @@ length(unique(c(unique(gt.full$OPERATOR_STREET_ADDRESS),
 
 length(unique(c(unique(gt.full$OPERATOR_ID), 
                 unique(gd.full$OPERATOR_ID),
-                unique(hl.full$OPERATOR_ID)))) #660? why different?
+                unique(hl.full$OPERATOR_ID)))) #660? why different? they moved?
 
 #### WRITING EXPORTS ####
   
 #csvs for each full table
-write_csv(gt.full, "data/clean/gt_inc.csv")
-write_csv(gd.full, "data/clean/gd_inc.csv")
-write_csv(hl.full, "data/clean/hl_inc.csv")
+write_csv(gt.op, "data/clean/gt_inc.csv")
+write_csv(gd.op, "data/clean/gd_inc.csv")
+write_csv(hl.op, "data/clean/hl_inc.csv")
 
 
 #csv for abridged all incidents
 write_csv(all.inc, "data/clean/all_inc.csv")
 
 #csv for mileage numbers 
-write_csv(miles, "data/clean/sys_miles.csv")
+write_csv(miles.op, "data/clean/sys_miles.csv")
 
 #csv for op match 
 write.csv(ops, "data/clean/operator_id_match.csv")
